135e688838fe546661e3e9fabbf01b1bf84ed839
CJ Miller <millerc5@rose-hulman.edu>
2016-02-08 16:25:34 -0500

    In class work

---
+++ Java-UML-GUI/JsonConfig.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Java_UML_GUI
+{
+    public class JsonConfig
+    {
+        public String InputFolder;
+        public String InputClasses;
+        public String OutputDirectory;
+        public String DotPath;
+        public String Phases;
+        public int Adapter_MethodDelegation;
+        public int Decorator_MethodDelegation;
+        public Boolean Singleton_RequireGetInstance;
+
+        public static JsonConfig loadFromFile(String path)
+        {
+            String toRead = File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<JsonConfig>(toRead);
+        }
+
+        public void saveToFile(String path)
+        {
+            String toWrtie = JsonConvert.SerializeObject(this);
+
+            File.WriteAllText(path, toWrtie);
+        }
+    }
+}
